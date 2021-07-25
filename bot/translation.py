af98aa5e3072da75f999abdc502ae80f069f6a5f
Mindmaste <87925802+Mindmaste@users.noreply.github.com>
2021-07-25 12:47:56 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -10,12 +10,7 @@ class Translation(object):
 
 <b>Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝</b>
 
-<b>Watch the video below to build an Pro Autofilter bot like me...</b>
 
-<b>Subscribe to the update channel to learn about my updates and activity...</b>
-
-<b>Press /help to know about available commands🤪</b>"""    
-    
     HELP_TEXT = """
 <b>😉How To Use Me!😉?</b>
 
@@ -46,18 +41,18 @@ class Translation(object):
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-<b><a href="https://t.me/Mo_Tech_YT">MoTechYT</a></b>
+<b><a href="https://t.me/MkvRockers">Mkvrockers</a></b>
 """
     
-    ABOUT_TEXT = """<b>➥ 📃Name</b> : <b>Pro Auto Filter Bot</b>
+    ABOUT_TEXT = """<b>➥ 📃Name</b> : <b>Mkv krithi shetty Bot</b>
     
-<b>➥ 😎Creator</b> : <b>@AlbertEinstein_TG</b> 
+<b>➥ 😎Creator</b> : <b>@Thalapathy_off</b> 
 
-<b>➥ 👨‍💻Editor</b> : <b>@Mrk_YT</b>
+<b>➥ 👨‍💻Editor</b> : <b>@Vip_offical</b>
 
 <b>➥ 🗣️Language</b> : <b>Python3</b>
 
 <b>➥ 📚Library</b> : <b><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></b>
 
-<b>➥ 📖Source Code</b> : <b><a href="https://github.com/MRK-YT/Pro-Auto-Filter-Bot">💥Click Me</a></b>
+<b>➥ Channel 👥</b> : <b><a href="https://t.me/MkvRockers">💥Click Me</a></b>
 """
